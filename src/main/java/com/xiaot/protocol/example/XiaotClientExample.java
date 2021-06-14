262bf265df10cce212383a2dd1e3b21a7fadcb6d
1572501217@qq.com <lzy@git.com>
2021-06-14 20:16:52 +0800

    commit

--- src/main/java/com/xiaot/protocol/example/XiaotClientExample.java
+++ src/main/java/com/xiaot/protocol/example/XiaotClientExample.java
@@ -24,60 +24,42 @@ public class XiaotClientExample {
         XiaotClient client = new XiaotClient("localhost", 9000);
 
         //客户端启动三条线程同时通过小T协议客户端向服务端发送数据
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        TimeUnit.MILLISECONDS.sleep(1000);
-                        client.sendMessage("zhangsan sayHello33333 .................................", null, new GenericFutureListener<Future<? super Void>>() {
-                            @Override
-                            public void operationComplete(Future<? super Void> future) throws Exception {
-                                if (future.isSuccess()) {
-//                                    log.info("send success...");
-                                } else {
-                                    log.error("send fail...", future.cause());
-                                }
-                            }
-                        });
-                    } catch (Exception e) {
-                        log.error(e.getMessage(), e);
-                    }
-                }
-            }
-        }).start();
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true){
-                    try {
-                        TimeUnit.MILLISECONDS.sleep(7);
-                        client.sendMessage("wangwu sayHello11111 .................................", null);
-                    }catch (Exception e){
-                        log.error(e.getMessage(), e);
-                    }
-                }
-            }
-        }).start();
-
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true){
-                    try {
-                        TimeUnit.MILLISECONDS.sleep(2);
-                        client.sendMessage("zhaoliu sayHello222222 .................................", null);
-                    }catch (Exception e){
-                        log.error(e.getMessage(), e);
-                    }
-                }
-            }
-        }).start();
+        new Thread(new Work(client, 20)).start();
+        new Thread(new Work(client, 30)).start();
+        new Thread(new Work(client, 50)).start();
 
         //阻塞主线程
         TimeUnit.HOURS.sleep(10000);
     }
 
+    public static class Work implements Runnable {
+
+        XiaotClient client;
+        long sleep;
+        public Work(XiaotClient client, long sleep){
+            this.client = client;
+            this.sleep = sleep;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(sleep);
+                    client.sendMessage(sleep + " zhangsan sayHello .................................", null, new GenericFutureListener<Future<? super Void>>() {
+                        @Override
+                        public void operationComplete(Future<? super Void> future) throws Exception {
+                            if (future.isSuccess()) {
+//                                    log.info("send success...");
+                            } else {
+                                log.error("send fail...", future.cause());
+                            }
+                        }
+                    });
+                } catch (Exception e) {
+                    log.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
 }
